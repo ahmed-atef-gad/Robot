42831382a11ac4f4ed3e763fa8b28601c4e7fed0
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2024-04-30 02:27:43 +0300

    update manual mode

--- Robot_uno/src/manual.cpp
+++ Robot_uno/src/manual.cpp
@@ -23,11 +23,21 @@ void manual()
         }
         else if (command == '3')
         {
-            Tleft();
+            analogWrite(RMOTOR_vel, 150);
+            analogWrite(LMOTOR_vel, 150);
+            digitalWrite(MOTOR_R1, HIGH);
+            digitalWrite(MOTOR_R2, LOW);
+            digitalWrite(MOTOR_L1, LOW);
+            digitalWrite(MOTOR_L2, HIGH);
         }
         else if (command == '4')
         {
-            Tright();
+            analogWrite(RMOTOR_vel, 150);
+            analogWrite(LMOTOR_vel, 150);
+            digitalWrite(MOTOR_R1, LOW);
+            digitalWrite(MOTOR_R2, HIGH);
+            digitalWrite(MOTOR_L1, HIGH);
+            digitalWrite(MOTOR_L2, LOW);
         }
         else if (command == '-')
         {
